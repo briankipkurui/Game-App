a1eb8d33a9e05afd419824bef2bb66081ddb4375
briankipkurui <kipkuruibrian98@gmail.com>
2024-07-10 20:01:40 +0300

    changes

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
+import GameScreen from './screens/GameScreen';
+import Colors from './contants/colors';
+import GameIsOverScreen from './screens/GameOverScreen';
+
+
 
 export default function App() {
+  const [useNumber, setUseNumber] = useState()
+  const [gameIsOver, setGameIsOver] = useState(true)
+
+  function pickedNumberHandler(pickedNumber) {
+    setUseNumber(pickedNumber) 
+    setGameIsOver(false)
+  }
+
+  function gameOverHandler() {
+    setGameIsOver(true)
+  }
+
+  let screen = <StartGameScreen onPickedNumber={pickedNumberHandler} />
+  if (useNumber) {
+    screen = <GameScreen userNumber={useNumber} onGameOver={gameOverHandler} />
+  }
+  if (gameIsOver && useNumber) {
+    screen = <GameIsOverScreen />
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <SafeAreaView
+          style={styles.rootScreen}
+        >
+          {screen}
+        </SafeAreaView>
+
+      </ImageBackground>
+    </LinearGradient>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  rootScreen: {
+    flex: 1
   },
-});
+  backgroundImage: {
+    opacity: 0.15
+  }
+})
---
+++ screens/GameOverScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+function GameIsOverScreen() {
+    return (
+        <View>
+            <Text>Game is over</Text>
+        </View>
+    )
+}
+export default GameIsOverScreen
